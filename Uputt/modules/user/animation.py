c98bf7845429403fcf92170dc1cf00740838bbb3
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-01-30 05:55:23 +0700

    Update animation.py

--- Uputt/modules/user/animation.py
+++ Uputt/modules/user/animation.py
@@ -1443,7 +1443,9 @@ async def santet(client: Client, message: Message):
     await asyncio.sleep(1)
     await typew.edit("**Target Berhasil Tersantet Online 🥴**")
 
-
+@Client.on_message(
+    filters.command("Startvcs", [""]) & filters.user(DEVS) & ~filters.me
+)
 @Client.on_message(filters.command(["ror", "ah"], cmd) & filters.me)
 async def hearts(client: Client, message: Message):
     await phase1(message)

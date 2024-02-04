1775a433100e8e24d3d80876804f07dad9dd6fc9
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-02-04 22:00:11 +0700

    Update __main__.py

--- Uputt/__main__.py
+++ Uputt/__main__.py
@@ -11,7 +11,7 @@ from Uputt.helpers.misc import create_botlog, heroku
 BOT_VER = "2.0.0"
 CMD_HANDLER = ["." "?" "!" "*"]
 MSG_ON = """
-〆 **Dareen-Userbot Di Aktifkan** 〆
+〆 **Dareen-Userbot Dah Aktif Ngentod** 〆
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 ㋱ **Userbot Version -** `{}`
 ㋱ **Ketik** `{}alive` **untuk Mengecek Bot**

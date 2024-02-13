a0b0003388b53ad2d0d80331e962a8f9e02646ff
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-02-13 09:32:16 +0700

    Update broadcast.py

--- Uputt/modules/user/broadcast.py
+++ Uputt/modules/user/broadcast.py
@@ -115,7 +115,7 @@ async def blchatgcast(client: Client, message: Message):
         await edit_or_reply(message, "🔮 **Blacklist GCAST:** `Disabled`")
 
 
-@Client.on_message(filters.command("addblacklist", cmd) & filters.me)
+@Client.on_message(filters.command("addbl", cmd) & filters.me)
 async def addblacklist(client: Client, message: Message):
     Man = await edit_or_reply(message, "`Processing...`")
     if HAPP is None:
@@ -143,7 +143,7 @@ async def addblacklist(client: Client, message: Message):
     restart()
 
 
-@Client.on_message(filters.command("delblacklist", cmd) & filters.me)
+@Client.on_message(filters.command("delbl", cmd) & filters.me)
 async def delblacklist(client: Client, message: Message):
     Man = await edit_or_reply(message, "`Processing...`")
     if HAPP is None:
@@ -183,11 +183,11 @@ add_command_help(
             "Untuk Mengecek informasi daftar blacklist gcast.",
         ],
         [
-            "addblacklist",
+            "addbl",
             "Untuk Menambahkan grup tersebut ke blacklist gcast.",
         ],
         [
-            "delblacklist",
+            "delbl",
             f"Untuk Menghapus grup tersebut dari blacklist gcast.\n\n  •  **Note : **Ketik perintah** `{cmd}addblacklist` **dan** `{cmd}delblacklist` **di grup yang kamu Blacklist.",
         ],
     ],

409af77982184d18106d17c9a845d1578dbdde53
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-02-14 07:21:52 +0700

    Update toxic1.py

--- Uputt/modules/user/toxic1.py
+++ Uputt/modules/user/toxic1.py
@@ -35,10 +35,35 @@ async def ngejamet(client: Client, message: Message):
     await xx.edit("**BYE MANUSIA HINA YANG LAHIR DI KELUARGA MISKIN DAN BURUK RUPA**")
     await asyncio.sleep(1.5)
 
+@Client.on_message(filters.command("idiot", cmd) & filters.me)
+async def ngejamet(client: Client, message: Message):
+    user_id = await extract_user(message)
+    if user_id in DEVS:
+        return await edit_or_reply(
+            message, "**Perintah ini Dilarang digunakan Kepada Developer Saya**"
+        )
+    xx = await edit_or_reply(message, "**WOYY**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**WOY IDIOOTT**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**LUU TUH IDIOT ANJENG**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**LU NGAPAIN DI SINI KONTOL**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**DI SINI GAK NERIMA ORANG IDIOT KAYA LO NGENTOT**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**CABUT AJA SANA KAU ANAK PEREK**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**GAK ADA GUNYA KAU DI SINI BANGSAT**")
+    await asyncio.sleep(1.5)
+    await xx.edit("**DADAH ANAK IDIOT YANG GOBLOK NYA SEJAGAT RAYA**")
+    await asyncio.sleep(1.5)
+
 
 add_command_help(
     "toxic1",
     [
         ["ank", "Untuk Menghakimi Anak Haram Macam Kau"],
+        ["idiot, "Untuk Ngatain Bocah Idiot Kaya Lu"].
     ],
 )

ac26cc30d0ee97b683723b39e113a60bb15d85a8
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-02-20 11:41:24 +0700

    Update help.py

--- Uputt/modules/user/help.py
+++ Uputt/modules/user/help.py
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @dareensupport × @skandallgua •"
+            message, f"```{str(ac)}```\n• @darensupport × @skandallgua •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"

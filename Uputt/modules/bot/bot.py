abb4826a2cc04b5569e7cd4e00f6e79bf6378a16
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-04-15 15:05:18 +0700

    Update bot.py

--- Uputt/modules/bot/bot.py
+++ Uputt/modules/bot/bot.py
@@ -65,7 +65,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     this_command = f"──「 **Help For {str(modul_name).upper()}** 」──\n\n"
     for x in commands:
         this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-    this_command += "© @skandallgua"
+    this_command += "© @cehadaren"
     bttn = [
         [InlineKeyboardButton(text="Return", callback_data="reopen")],
     ]
